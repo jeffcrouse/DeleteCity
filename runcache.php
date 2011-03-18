771f5c248fb4c51204b7fba7e7cb50e951af2c8e
Jeff Crouse <jefftimesten@gmail.com>
2011-03-17 22:04:34 -0400

    Started to add progress stuff to runcache.

--- runcache.php
+++ runcache.php
@@ -13,6 +13,8 @@ foreach($urls as $url)
 	$xmlstr = get_web_page( $url );
 	$xmldoc = new SimpleXMLElement( $xmlstr['content'] );
 	
+	$num_vids = count($xmldoc->entry);
+	$i=1;
 	foreach($xmldoc->entry as $entry)
 	{
 		$vid_url = $entry->link[0]['href'];				// TO DO:  We can't be sure that the href is element 0
@@ -29,40 +31,42 @@ foreach($urls as $url)
     
 		if($result->numRows() > 0)
 		{
-			print "Status: $youtube_id already exists in database.  Skipping.\n";
+			print "Status: [$i/$num_vids] $youtube_id already exists in database.  Skipping.\n";
 			if(!file_exists("{$cache_dir}/{$youtube_id}.mp4"))
 			{
 				print "Error: Record exists in database, but video file doesn't exist!";
 			}
-			continue;
-		}
-
-		$title = $entry->title;
-		$published = $entry->published;
-		$updated = $entry->updated;
-		$author = $entry->author->name;
-		print "Status: Downloading \"$title\" ($youtube_id)\n";
-		
-		// http://rg3.github.com/youtube-dl/documentation.html#d6
-		`youtube-dl/youtube-dl --continue --no-overwrites --ignore-errors --format=18 --output="{$cache_dir}/%(id)s.%(ext)s" --rate-limit=$rate_limit $vid_url`;
-		
-		if(file_exists("{$cache_dir}/{$youtube_id}.mp4"))
-		{
-			print "Status: Adding \"$title\" ({$youtube_id}) to database\n";
-			$sql=sprintf("INSERT INTO videos (youtube_id, title, author, date_added, date_updated) 
-				VALUES ('%s', '%s', '%s', DATETIME('now'), DATETIME('now'))",
-				sqlite_escape_string($youtube_id),
-				sqlite_escape_string($title),
-				sqlite_escape_string($author)
-			);
-			$db->query($sql, SQLITE_ASSOC, $query_error);
-			if ($query_error)
-				die("Error: $query_error\n"); 
 		}
 		else
 		{
-			print "Error: File was not successfully downloaded.  Not adding to database.";
+			$title = $entry->title;
+			$published = $entry->published;
+			$updated = $entry->updated;
+			$author = $entry->author->name;
+			print "Status: [$i/$num_vids] Downloading \"$title\" ($youtube_id)\n";
+			
+			// http://rg3.github.com/youtube-dl/documentation.html#d6
+			`youtube-dl/youtube-dl --continue --no-overwrites --ignore-errors --format=18 --output="{$cache_dir}/%(id)s.%(ext)s" --rate-limit=$rate_limit $vid_url`;
+			
+			if(file_exists("{$cache_dir}/{$youtube_id}.mp4"))
+			{
+				print "Status: Adding \"$title\" ({$youtube_id}) to database\n";
+				$sql=sprintf("INSERT INTO videos (youtube_id, title, author, date_added, date_updated) 
+					VALUES ('%s', '%s', '%s', DATETIME('now'), DATETIME('now'))",
+					sqlite_escape_string($youtube_id),
+					sqlite_escape_string($title),
+					sqlite_escape_string($author)
+				);
+				$db->query($sql, SQLITE_ASSOC, $query_error);
+				if ($query_error)
+					die("Error: $query_error\n"); 
+			}
+			else
+			{
+				print "Error: File was not successfully downloaded.  Not adding to database.";
+			}
 		}
+		$i++;
 	}
 }
 ?>
